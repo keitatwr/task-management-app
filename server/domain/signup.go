d0b856bfee0c25d1e259ade4f70bc2c621dc91de
keitatwr <keita.twr@gmail.com>
2024-11-28 17:41:09 +0900

    Add type SignupRequest

--- server/domain/signup.go
+++ server/domain/signup.go
@@ -6,3 +6,9 @@ type SignupUsecase interface {
 	Create(ctx context.Context, name, email, password string) error
 	GetUserByEmail(ctx context.Context, email string) (*User, error)
 }
+
+type SignupRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
