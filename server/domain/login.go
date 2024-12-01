e3d7fe07927ae4c083e0ffa0578da2f81aafcb29
keitatwr <keita.twr@gmail.com>
2024-12-01 13:23:53 +0900

    Add func GenerateSession to LoginUsecase & Add model LoginRequest

--- server/domain/login.go
+++ server/domain/login.go
@@ -1,7 +1,17 @@
 package domain
 
-import "context"
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+)
 
 type LoginUsecase interface {
 	GetUserByEmail(context.Context, string) (*User, error)
+	GenerateSession(*gin.Context, User) error
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
