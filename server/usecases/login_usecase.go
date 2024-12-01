9a4fb1b735c5144c8692c17a73671c86e193bc88
keitatwr <keita.twr@gmail.com>
2024-12-01 13:25:12 +0900

    Add func GenerateSession

--- server/usecases/login_usecase.go
+++ server/usecases/login_usecase.go
@@ -2,8 +2,11 @@ package usecases
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
 	"github.com/keitatwr/todo-app/domain"
 )
 
@@ -19,10 +22,24 @@ func NewLoginUsecase(ur domain.UserRepository, timeout time.Duration) domain.Log
 	}
 }
 
-func (lu loginUsecase) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+func (lu *loginUsecase) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
 	user, err := lu.userRepository.GetUserByEmail(ctx, email)
 	if err != nil {
 		return nil, err
 	}
 	return user, nil
+}
+
+func (lu *loginUsecase) GenerateSession(ctx *gin.Context, user domain.User) error {
+	session := sessions.Default(ctx)
+	bUser, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+	session.Set("userInfo", string(bUser))
+	session.Options(sessions.Options{MaxAge: 3600, Path: "/", HttpOnly: false})
+	if err := session.Save(); err != nil {
+		return err
+	}
+	return nil
 }
