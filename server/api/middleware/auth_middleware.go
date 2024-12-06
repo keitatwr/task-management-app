779f9d9e1ce80626a9044f9ab84aab4e46d524ab
keitatwr <keita.twr@gmail.com>
2024-12-06 14:57:54 +0900

    Add context handling

--- server/api/middleware/auth_middleware.go
+++ server/api/middleware/auth_middleware.go
@@ -1,12 +1,28 @@
 package middleware
 
 import (
+	"context"
 	"encoding/json"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/keitatwr/todo-app/domain"
 )
+
+var userContextKey = struct{}{}
+
+func SetUserContext(c *gin.Context, user domain.User) {
+	ctx := context.WithValue(c.Request.Context(), userContextKey, user)
+	c.Request = c.Request.WithContext(ctx)
+}
+
+func GetUserContext(c *gin.Context) *domain.User {
+	user, ok := c.Request.Context().Value(userContextKey).(domain.User)
+	if !ok {
+		return nil
+	}
+	return &user
+}
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -23,6 +39,7 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+		SetUserContext(c, authUser)
 		c.Next()
 	}
 }
