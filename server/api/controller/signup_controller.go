e27d075184082bb69b8ff58d9250efb3edbf438f
keitatwr <keita.twr@gmail.com>
2024-12-26 13:36:35 +0900

    Add handle validation error and handle create error

--- server/api/controller/signup_controller.go
+++ server/api/controller/signup_controller.go
@@ -1,10 +1,20 @@
 package controller
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/keitatwr/task-management-app/api/response"
 	"github.com/keitatwr/task-management-app/domain"
+	"github.com/keitatwr/task-management-app/internal/logger"
+	"github.com/keitatwr/task-management-app/internal/myerror"
 	"github.com/keitatwr/task-management-app/internal/security"
 )
 
@@ -17,38 +27,91 @@ func (sc *SignupController) Signup(c *gin.Context) {
 	// binding json request
 	var request domain.SignupRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
-		// logger.Warnf(c.Request.Context(), "invalid request payload: %v", err)
-		c.JSON(http.StatusBadRequest, domain.ErrorResponse{
-			Message: "invalid request payload"})
+		sc.handleValidationError(c, err)
 		return
 	}
 
-	// 	// check if user already exists
-	// 	_, err := sc.SignupUsecase.GetUserByEmail(c, request.Email)
-	// 	if err == nil {
-	// 		logger.Warnf(c.Request.Context(), "email %s already exists", request.Email)
-	// 		c.JSON(http.StatusConflict, domain.ErrorResponse{
-	// 			Message: fmt.Sprintf("email %s already exists", request.Email)})
-	// 		return
-	// 	}
+	// check if user already exists
+	_, err := sc.SignupUsecase.FetchUserByEmail(c, request.Email)
+	if err == nil {
+		appErr := myerror.ErrUserAlreadyExists.WrapWithDescription(err, fmt.Sprintf("email '%s' is already exists", request.Email))
+		logger.W(c.Request.Context(), "user already exsits", appErr)
+		response.Error(c, http.StatusConflict, "user already exists", appErr)
+		return
+	}
 
-	// 	// hash password
-	// 	request.Password, err = sc.PasswordHasher.HashPassword(request.Password)
-	// 	if err != nil {
-	// 		logger.Errorf(c.Request.Context(), "failed to hash password: %v", err)
-	// 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{
-	// 			Message: "failed to hash password"})
-	// 		return
-	// 	}
+	// hash password
+	request.Password, err = sc.PasswordHasher.HashPassword(request.Password)
+	if err != nil {
+		appErr := myerror.ErrHashPassword.WrapWithDescription(err, "failed to hash password")
+		logger.W(c.Request.Context(), "failed to hash password", appErr)
+		response.Error(c, http.StatusInternalServerError, "failed to hash password", appErr)
+		return
+	}
 
-	// 	// create user
-	// 	err = sc.SignupUsecase.Create(c, request.Name, request.Email, request.Password)
-	// 	if err != nil {
-	// 		logger.Errorf(c.Request.Context(), "failed to create user: %v", err)
-	// 		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{
-	// 			Message: fmt.Sprintf("failed to create user: %v", err)})
-	// 		return
-	// 	}
+	// create user
+	err = sc.SignupUsecase.Create(c, request.Name, request.Email, request.Password)
+	if err != nil {
+		sc.handleCreateError(c, err)
+		return
+	}
 
-	// c.JSON(http.StatusCreated, domain.SuccessResponse{Message: "user created"})
+	c.JSON(http.StatusCreated, domain.SuccessResponse{Message: "user created"})
+}
+
+func (sc *SignupController) handleValidationError(c *gin.Context, err error) {
+	var vErr *myerror.AppError
+
+	switch e := err.(type) {
+	case validator.ValidationErrors:
+		missingFields := []string{}
+		for _, fieldErr := range e {
+			missingFields = append(missingFields, fieldErr.Field())
+		}
+		vErr = myerror.ErrValidation.WrapWithDescription(e,
+			fmt.Sprintf("missing fields: %v", strings.Join(missingFields, ", ")))
+
+	case *json.UnmarshalTypeError:
+		vErr = myerror.ErrValidation.WrapWithDescription(e,
+			fmt.Sprintf("missing field type: %v, expect: %s, actual: %s", e.Field, e.Type, e.Value))
+
+	case *json.SyntaxError:
+		vErr = myerror.ErrValidation.WrapWithDescription(e,
+			fmt.Sprintf("json syntax error, offset: %d", e.Offset))
+
+	case *time.ParseError:
+		vErr = myerror.ErrValidation.WrapWithDescription(e,
+			fmt.Sprintf("time parse error, expect format: %s", "yyyy-mm-dd"))
+
+	case *strconv.NumError:
+		vErr = myerror.ErrValidation.WrapWithDescription(e,
+			"string convert error, expect format: number")
+
+	default:
+		vErr = myerror.ErrUnExpected.WithDescription(err.Error())
+	}
+
+	if vErr != nil {
+		logger.W(c.Request.Context(), "occurred validation error", vErr)
+		response.Error(c, http.StatusBadRequest, "your request is validation failed", vErr)
+	}
+
+}
+
+func (sc *SignupController) handleCreateError(c *gin.Context, err error) {
+	ctx := c.Request.Context()
+
+	var appErr *myerror.AppError
+	if errors.As(err, &appErr) {
+		switch {
+		case errors.Is(appErr, myerror.ErrQueryFailed):
+			err := appErr.WithDescription("failed to execute query")
+			logger.E(ctx, "occurred create user eroror", err)
+			response.Error(c, http.StatusInternalServerError, "failed to create user", err)
+			return
+		default:
+			logger.W(ctx, "failed to create user", err)
+			response.Error(c, http.StatusInternalServerError, "failed to create user", err)
+		}
+	}
 }
